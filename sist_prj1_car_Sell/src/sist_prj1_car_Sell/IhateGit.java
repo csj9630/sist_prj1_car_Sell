21344611e75aedbac2143f657e5b96f49c6558e6
csj1972 <csj9630@gmail.com~>
2025-10-26 21:26:31 +0900

    첫 커밋

---
+++ sist_prj1_car_Sell/src/sist_prj1_car_Sell/IhateGit.java
@@ -0,0 +1,10 @@
+package sist_prj1_car_Sell;
+
+public class IhateGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("진짜 성가신 프로그램이네 참");
+	}
+
+}
