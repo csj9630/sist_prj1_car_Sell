52bb8ab9204fbc5721b41342c22e2480ececb744
csjSist <csj9630@gmail.com>
2025-10-27 11:58:34 +0900

    테스트 usercode 위치 수정

--- src/kr/co/sist/car_sell/design/UserMenu/UserMenuDesign.java
+++ src/kr/co/sist/car_sell/design/UserMenu/UserMenuDesign.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import kr.co.sist.car_sell.event.UserMenu.UserMenuEvt;
 
@@ -20,81 +21,72 @@ import kr.co.sist.car_sell.event.UserMenu.UserMenuEvt;
 public class UserMenuDesign extends JFrame {
 
 	private JButton jbtnModifyInfo, jbtnModifyPw, jbtnOrderList;
-	private int user_code=1; //사용자 코드
+	private int user_code = 1; // 사용자 코드
 
 	public UserMenuDesign() {
 		super("사용자 정보");
 		JLabel jlTitle = new JLabel("내 정보");
-		
-		
+
 		JPanel jpCenter = new JPanel();
 		JPanel jpNorth = new JPanel();
-		
-		
-		
-		//버튼 객체 생성
+
+		// 버튼 객체 생성
 		jbtnModifyInfo = new JButton("내 정보 수정");
 		jbtnModifyPw = new JButton("비밀번호 수정");
 		jbtnOrderList = new JButton("주문 내역 ");
-		
-		//상단 패널에 타이틀 추가
+
+
+		// 상단 패널에 타이틀 추가
 		jpNorth.add(jlTitle);
-		
-		//센터 패널에 버튼 추가
+
+		// ㅁㅁㅁㅁㅁㅁ테스트용 usercode 출력 ㅁㅁㅁㅁㅁㅁㅁㅁ
+		JLabel jpUserCode = new JLabel("UserCode = "+user_code);
+		jpUserCode.setFont(new Font("맑은고딕", Font.BOLD, 20));
+		add("South",jpUserCode);
+
+		// 센터 패널에 버튼 추가
 		jpCenter.add(jbtnModifyInfo);
 		jpCenter.add(jbtnModifyPw);
 		jpCenter.add(jbtnOrderList);
-		
-		//ㅁㅁㅁㅁㅁㅁ테스트용 usercode 출력 ㅁㅁㅁㅁㅁㅁㅁㅁ
-		JLabel jpUserCode = new JLabel("UserCode = "+user_code);
-		jpCenter.add(jpUserCode);
-		
-		
-		//FlowLayout으로 설정
+
+		// FlowLayout으로 설정
 		jpCenter.setLayout(new FlowLayout());
-		
-		
-		//폰트 설정
-		Font fontTitle = new Font("맑은고딕",Font.BOLD,40);
-		Font fontBtn = new Font("맑은고딕",Font.BOLD,20);
+
+		// 폰트 설정
+		Font fontTitle = new Font("맑은고딕", Font.BOLD, 40);
+		Font fontBtn = new Font("맑은고딕", Font.BOLD, 20);
 
 		jlTitle.setFont(fontTitle);
 		jbtnModifyInfo.setFont(fontBtn);
 		jbtnModifyPw.setFont(fontBtn);
 		jbtnOrderList.setFont(fontBtn);
-		
-		jbtnModifyInfo.setLocation(500,500);
-	
 
-		
-		//JPanel에 상하 여백 넣기
-		//----createEmptyBorder(int top, int left, int bottom, int right)
-		jpNorth.setBorder(BorderFactory.createEmptyBorder(10,10,10, 10));
-		jpCenter.setBorder(BorderFactory.createEmptyBorder(100,10, 100, 10));
-		
-		//JPanel에 배경색 넣기
+		jbtnModifyInfo.setLocation(500, 500);
+
+		// JPanel에 상하 여백 넣기
+		// ----createEmptyBorder(int top, int left, int bottom, int right)
+		jpNorth.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		jpCenter.setBorder(BorderFactory.createEmptyBorder(100, 10, 100, 10));
+
+		// JPanel에 배경색 넣기
 		jpNorth.setBackground(Color.white);
 		jpCenter.setBackground(Color.white);
-		
-		
-		add("North",jpNorth);
-		add("Center",jpCenter);
-		
-		
-		//이벤트 등록
+
+		add("North", jpNorth);
+		add("Center", jpCenter);
+
+		// 이벤트 등록
 		UserMenuEvt ume = new UserMenuEvt(this);
-		
+
 		jbtnModifyInfo.addActionListener(ume);
 		jbtnModifyPw.addActionListener(ume);
 		jbtnOrderList.addActionListener(ume);
-		
+
 		addWindowListener(ume);
-		
-		
-		
-		//화면 설정
-		setBounds(600,250,600,500);
-		setLocationRelativeTo(this); //윈도우 창을 화면 가운데에 띄우는 역할을 함
+
+		// 화면 설정
+		setBounds(600, 250, 600, 500);
+		setLocationRelativeTo(this); // 윈도우 창을 화면 가운데에 띄우는 역할을 함
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -116,6 +108,4 @@ public class UserMenuDesign extends JFrame {
 		return user_code;
 	}
 
-
-}//class
-
+}// class
