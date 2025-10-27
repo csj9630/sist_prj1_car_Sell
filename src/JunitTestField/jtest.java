2acaf7aa5bf5525405883024e99f422ebe4e1c0e
csj1972 <csj9630@gmail.com~>
2025-10-27 22:49:41 +0900

    사용자 로그인 insert 완료, selectone은 그대로 쓰자.

--- src/JunitTestField/jtest.java
+++ src/JunitTestField/jtest.java
@@ -1,9 +1,16 @@
 package JunitTestField;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import kr.co.sist.car_sell.dao.UserDAO;
+import kr.co.sist.car_sell.dto.UserDTO;
 
 
 class jtest {
@@ -13,13 +20,25 @@ class jtest {
 //		fail("Not yet implemented");
 //	}
 	
-	@DisplayName("Test1")
+	@DisplayName("insertTest")
 	@Test
 	void connectionTest() {
 
-		String pw = "qwer1234@";
-//		boolean flag = PasswordValidator.isValid(pw);
-
+		UserDTO udto = new UserDTO("asdf7894","q1w2e3r4","성진우","asdf@naver.com","010-7889-9988","경상북도 고령군 성산면 고탄길 37-1","8888-8888-8888-8888");
+		UserDAO udao = UserDAO.getInstance();
+		int result =0;
+		try {
+			result=udao.insertUser(udto);
+			assertEquals(result, 1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 //		assertNotNull(flag);
 	}
 
